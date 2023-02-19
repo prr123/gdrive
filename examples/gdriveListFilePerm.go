c8fcc48b77c8edd1bac65e45e856d8629e28ca51
prr123 <priemen@gmail.com>
2023-02-19 15:27:28 +0100

    initial commit

---
+++ examples/gdriveListFilePerm.go
@@ -0,0 +1,59 @@
+// golang program to test gdrive
+// author: prr, azul software
+// created: 6/2/2023
+// copyright 2023 prr, Peter Riemenschneider, Azul Software
+//
+//
+
+package main
+
+import (
+        "fmt"
+        "os"
+//		"io"
+		gdrive "google/gdrive/examples/gdriveLib"
+)
+
+
+func main() {
+
+/*
+	numArgs := len(os.Args)
+	if numArgs <2 {
+		fmt.Printf("no enough arguments -- need folder name as second argument!\n")
+		os.Exit(-1)
+	}
+
+	foldNam := os.Args[1]
+*/
+	gd, err := gdrive.InitDriveApi()
+	if err != nil {
+		fmt.Printf("Gdrive Api Init error: %v\n", err)
+		os.Exit(1)
+	}
+
+// folder
+//	azulTestId :="1cRgugvok058kLc8Nxbg5vZaum0HbDAUkkIcrn29WY4P45PLbA-kuW4N_NteQeJZwBICBEppW"
+
+	docId := "1GjD9109eAAfufreM6Oj1EZpyvi6BHtCX0ihQSsKrloU"
+	fmt.Printf("Id: %s\n",docId)
+
+	filInfo,err := gd.GetFilInfoById(docId)
+	if err != nil {
+		fmt.Printf("error GetFilInfo: %v\n", err)
+		os.Exit(-1)
+	}
+
+	gdrive.PrintFilInfo(filInfo)
+
+	permList, err := gd.ListFilePerm(filInfo.Id)
+	if err != nil {
+		fmt.Printf("error ListFilePerm: %v\n", err)
+		os.Exit(-1)
+	}
+
+	gdrive.PrintPermList(*filInfo, permList)
+
+	fmt.Println("Success!")
+
+}
